991d8697e08e489d025aa15cd9489f1899ea9871
John America <john.america@example.com>
2026-04-02 21:52:03 -0500

    added

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const PORTFOLIO_GAMES = [
     { placeId: 136577413998809, role: 'Beta Tester', description: 'Tested mechanics and reported physics interaction bugs in flight mechanics during beta test.' }, // build plane
     { placeId: 109932080383306, role: 'Tester', description: 'Evaluated core gameplay loops during alpha test.' }, // Slap Brawl!
     { placeId: 124910815181368, role: 'Beta Tester', description: 'Tested and helped resolve visual bugs during testing phase, and gave advice to improve core gameplay loop' }, // [pillow]
-    // { placeId: DevTest 91774484861138, role: 'Beta Tester', description: 'Tested and helped resolve visual bugs during testing phase, and gave advice to improve core gameplay loop' },
+    { placeId: 94702395375549, role: 'Trade Update Tester', description: 'Tested and helped find bugs pertaining to the trading system' },
     { placeId: 109021167563361, role: 'Tester', description: 'Helped identify functional bugs during beta test.' } //Build a tree factory
 ];
 
